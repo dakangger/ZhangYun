ce1da46d5f311e1fa0e1c352b85d3eb3d1fd85ec
kangyafei <kangyafei@rtd002demacbook-pro.local>
2016-09-18 11:25:48 +0800

    修改方法

--- ZhangYun/ViewController.m
+++ ZhangYun/ViewController.m
@@ -17,9 +17,14 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    [self createUI];
 }
 
+-(void)createUI
+{
 
+
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
