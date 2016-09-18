3e76e2380e28667bb6c64e3af798fda0fa43becd
kangyafei <kangyafei@rtd002demacbook-pro.local>
2016-09-18 10:55:57 +0800

    Initial Commit

---
+++ ZhangYun/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZhangYun
+//
+//  Created by kangyafei on 16/9/18.
+//  Copyright © 2016年 kangyafei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZhangYun/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZhangYun
+//
+//  Created by kangyafei on 16/9/18.
+//  Copyright © 2016年 kangyafei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
